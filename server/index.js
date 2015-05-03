3aeb71cfcac5c890f1ed4009d44e399a6f8248cf
Andrew Munro <andrew.munro@dubitlimited.com>
2015-05-03 12:02:43 +0100

    Code cleanup and converting to tabs.

--- server/index.js
+++ server/index.js
@@ -2,21 +2,21 @@ function MilkStompServer() {
 	var config = require('config');
 	var _ = require('lodash');
 	var io = require('socket.io')();
-	
+
 	var MilkSocket = require('net/MilkSocket');
 	var Player = require('player/Player');
 	var router = new (require('router/MilkRouter'))();
-	
+
 	var players = {};
 
 	function createPlayer(guid, socket) {
 		var player = new Player(guid, socket);
 
-		player.socket.on('data', function(message) {
+		player.socket.on('data', function (message) {
 			router.handle(message, player);
 		});
-		
-		player.socket.on('close', function() {
+
+		player.socket.on('close', function () {
 			if (players[guid] !== undefined) {
 				player.destroy();
 				delete players[guid];
@@ -25,16 +25,16 @@ function MilkStompServer() {
 
 		return player;
 	}
-	
-	this.boot = function() {
+
+	this.boot = function () {
 		io.listen(config.PORT, '0.0.0.0');
 
-		io.on('connection', function(socket) {
+		io.on('connection', function (socket) {
 			socket.setEncoding('utf8');
 			socket = new MilkSocket(socket);
 			players[socket.guid] = createPlayer(socket.guid, socket);
 		});
-		
+
 		console.log("Server Initialised");
 	}
 }
