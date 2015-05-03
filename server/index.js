45984c2f504ab5dc4780d25d6cd1e2a45334103d
Andrew Munro <andrew.munro@dubitlimited.com>
2015-05-03 01:53:29 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,44 @@
+function MilkStompServer() {
+	var config = require('config');
+	var _ = require('lodash');
+	var io = require('socket.io')();
+	
+	var MilkSocket = require('net/MilkSocket');
+	var Player = require('player/Player');
+	var router = new require('router/Router')();
+	
+	var players = {};
+
+	function createPlayer(guid, socket) {
+		var player = new Player(guid, socket);
+
+		player.socket.on('data', function(message) {
+			router.handle(message, player);
+		});
+		
+		player.socket.on('close', function() {
+			if (players[guid] !== undefined) {
+				player.destroy();
+				delete players[guid];
+			}
+		});
+
+		return player;
+	}
+	
+	this.boot = function() {
+		io.listen(config.PORT, '0.0.0.0');
+
+		io.on('connection', function(socket) {
+			socket.setEncoding('utf8');
+			socket = new MilkSocket(socket);
+			players[socket.guid] = createPlayer(socket.guid, socket);
+		});
+		
+		console.log("Server Initialised");
+	}
+}
+
+module.exports = MilkStompServer;
+
+new MilkStompServer().boot();
